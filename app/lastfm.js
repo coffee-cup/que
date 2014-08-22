9f6cd1a31c0b6219cf5c5b68b56533dcfc7f0617
coffee-cup <jakerunzer@gmail.com>
2014-08-21 17:35:29 -0700

    renamed musicjs to lastfmjs

--- app/lastfm.js
+++ app/lastfm.js
@@ -3,7 +3,7 @@ var log = require('../config/log');
 var secrets = require('../config/secrets')
 
 var API_ROOT   = 'http://ws.audioscrobbler.com/2.0/';
-var LIMIT      = 3;
+var LIMIT      = 10;
 
 module.exports.getMethod = function(method, params, callback) {
   params['api_key'] = secrets.LASTFM_API_KEY;
@@ -14,6 +14,7 @@ module.exports.getMethod = function(method, params, callback) {
   log.info('making api call to lastfm');
   request({url: API_ROOT, json: true, qs: params}, function (error, response, body) {
   if (!error && response.statusCode == 200) {
+    log.info('got response from lastfm');
     callback(body);
   }else {
     log.error('There was an error making call to lastfm');
